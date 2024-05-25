26130d55bc93bca5cd50eafd6ecc9b2f624e7761
adixxsx <ad1letsa5irkulov@gmail.com>
2024-05-24 17:07:32 -0700

    Initial Commit

---
+++ GeeksPro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeeksPro
+//
+//  Created by user on 24/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
